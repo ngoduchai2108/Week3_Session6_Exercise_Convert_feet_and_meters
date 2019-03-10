d4a9d0b9cebf7ed67dbef6dff076fd250d8c9944
ngoduchai2108 <hai2108hatayqo@gmail.com>
2019-03-10 22:31:25 +0700

    add file

---
+++ feet_to_meters.js
@@ -0,0 +1,3 @@
+function feetToMeters(n) {
+    return document.getElementById('result1').innerHTML=n+" feet = "+ 0.305*n+" m";
+}
---
+++ meters_to_feet.js
@@ -0,0 +1,3 @@
+function metersToFeet(n) {
+    return document.getElementById('result2').innerHTML=n+" m = "+ 3.279*n+" feet";
+}
